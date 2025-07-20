5d8bcac3dbf63ede5e117781616b6d94df79a860
Michael Schaecher <MichaelLeeSchaecher@gmail.com>
2025-07-19 21:46:28 -0600

    package: Added true debian directory for building.

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/grub-btrfsd/issues
+# Bug-Submit: https://github.com/<user>/grub-btrfsd/issues/new
+# Changelog: https://github.com/<user>/grub-btrfsd/blob/master/CHANGES
+# Documentation: https://github.com/<user>/grub-btrfsd/wiki
+# Repository-Browse: https://github.com/<user>/grub-btrfsd
+# Repository: https://github.com/<user>/grub-btrfsd.git
